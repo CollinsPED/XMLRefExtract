45ebcef6e9460c61d64cc2cef413265b7b89c52a
CollinsPED <kmcconigly@hotmail.com>
2020-02-20 16:52:06 +0000

    Initial commit of project files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace XMLRefExtract
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                XMLRefExtract processor = new XMLRefExtract();
+                processor.Launch();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ XMLRefExtract.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLRefExtract
+{
+    class XMLRefExtract
+    {
+        private static string xmlFilePath = "..\\..\\..\\InputDocument.xml";
+
+        public XMLRefExtract(){}
+
+        public void Launch()
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xmlFilePath);
+                XmlNode root = doc.FirstChild;
+
+                XmlNode decNode = root.FirstChild;
+
+                if (decNode.HasChildNodes)
+                {
+                    for (int i = 0; i < decNode.ChildNodes.Count; i++)
+                    {
+                        XmlNode headerNode = decNode.ChildNodes[i].ChildNodes[0];
+                        if (headerNode.HasChildNodes)
+                        {
+                            for (int j = 0; j < headerNode.ChildNodes.Count; j++)
+                            {
+                                XmlNode node = headerNode.ChildNodes[j];
+                                if (node.Attributes.Count > 0)
+                                {
+                                    foreach (XmlAttribute att in node.Attributes)
+                                    {
+                                        if (att.Name == "RefCode")
+                                        {
+                                            if ((att.InnerText == "MWB") || (att.InnerText == "TRV") || (att.InnerText == "CAR"))
+                                            {
+                                                Console.Write(att.InnerText + ": ");
+                                                Console.WriteLine(node.FirstChild.InnerText);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (XmlException xex)
+            {
+                Console.WriteLine(xex);   
+            }
+        }
+    }
+}
